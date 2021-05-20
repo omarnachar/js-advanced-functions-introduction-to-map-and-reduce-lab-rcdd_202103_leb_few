c078c38164c58205a5d7871722679fe2ab968216
Omar Nachar <omarnachar2@gmail.com>
2021-05-20 13:24:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function reduceToTotal(sourceArray, startingPoint = 0){
 
 function reduceToAllTrue(sourceArray){
 
-    const reducer = function(accumulator, currentValue){  
+    const reducer = function(accumulator, currentValue){
         if(!!accumulator == true && !!currentValue == true){
             return true;
         } else {
@@ -34,12 +34,12 @@ function reduceToAllTrue(sourceArray){
 }
 
 function reduceToAnyTrue(sourceArray){
-    const reducer = function(accumulator, currentValue){  
+    const reducer = function(accumulator, currentValue){
         if(accumulator == true){
             return true
         } else {
             return !!currentValue
-        } 
+        }
     }
     return sourceArray.reduce(reducer, false)
 }
